5209331e77c11328c7a73f76496f1e7d62442d43
Gurgen Verdian <gurgen.verdian@gmail.com>
2022-02-21 21:21:02 -0500

    adding samples

--- player.py
+++ player.py
@@ -1,20 +1,31 @@
 from tkinter import *
+from pygame import mixer
 
 window = Tk()
+
+mixer.init()
+
 window.geometry('300x300')
 window.title('Python music Player')
 
 textLabel = Label(window, text="This is a Play Button")
 textLabel.pack()
 
+LionKing = 'The Lion Sleeps Tonight.mp3'
+StarWars = 'Star Wars Theme Song By John Williams.ogg'
+ACDC = 'You shook me.mp3'
+Boston = 'More than a feeling.mp3'
+
+def play_music():
+    mixer.music.load(ACDC)
+    mixer.music.play()
+
+
+
 photo = PhotoImage(file='pensiveRobot.png')
-photoLabel = Label(window, image=photo)
-photoLabel.pack()
-
-
-
-
-
-
+# photoLabel = Label(window, image=photo, height=100, width=100)
+# photoLabel.pack()
+playButton = Button(window, image=photo, command=play_music)
+playButton.pack()
 
 window.mainloop()
