c64b7bb5b104213b5b3f0843d5585a79748b741b
Gurgen Verdian <gurgen.verdian@gmail.com>
2022-02-22 19:24:52 -0500

    adding play and stop funcs

--- player.py
+++ player.py
@@ -1,11 +1,11 @@
 from tkinter import *
 from pygame import mixer
 
+# initialize the mixer and window
+mixer.init()
 window = Tk()
 
-mixer.init()
-
-window.geometry('300x250')
+window.geometry('300x350')
 window.title('Python music Player')
 
 textLabel = Label(window, text="This is a Play Button")
@@ -17,15 +17,22 @@ ACDC = 'Media//You shook me.mp3'
 Boston = 'Media//More than a feeling.mp3'
 
 def play_music():
-    mixer.music.load(ACDC)
+    mixer.music.load(LionKing)
     mixer.music.play()
+    mixer.music.queue(ACDC)
 
-
+def stop_music():
+    mixer.music.stop()
 
 photo = PhotoImage(file='Media//Logo.png')
 # photoLabel = Label(window, image=photo, height=100, width=100)
 # photoLabel.pack()
 playButton = Button(window, image=photo, command=play_music)
 playButton.pack()
+
+#stop button
+stopPhoto = PhotoImage(file='Media//stop.png')
+stopButton = Button(window, image=stopPhoto, height=50, width=50, command=stop_music)
+stopButton.pack()
 
 window.mainloop()
